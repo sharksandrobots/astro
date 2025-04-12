2251ee73a919b331d8735c55a47556150460a754
Mike Bowser <mbowser@gmail.com>
2025-04-12 16:59:42 -0400

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,3 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  output: 'static',
-  // If you've customized the output directory, check it here
-  outDir: './dist'
-});
+export default defineConfig({});
